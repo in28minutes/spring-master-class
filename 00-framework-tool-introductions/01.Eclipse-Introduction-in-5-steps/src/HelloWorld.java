3500265ad3b6d6641972538ecbab5ad12adc6ed3
Ishkhan Zargarian <izargarian@yahoo.com>
2020-07-30 21:51:26 -0400

    added a comment line

--- 00-framework-tool-introductions/01.Eclipse-Introduction-in-5-steps/src/HelloWorld.java
+++ 00-framework-tool-introductions/01.Eclipse-Introduction-in-5-steps/src/HelloWorld.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
-
+// comment for test by Ishkhan
 public class HelloWorld {
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<String, String>();
