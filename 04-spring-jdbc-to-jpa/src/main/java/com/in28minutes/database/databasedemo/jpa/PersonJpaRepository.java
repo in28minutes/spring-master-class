70bd36dff6a29fb38da59277159b7fe8cdecab69
bsmahi <bs.mahi@gmail.com>
2022-05-03 19:53:18 +0530

    Changed Spring Boot Parent Version
    1. In pom.xml changed to 3.0.0-M2 and Java version to 17
    2. In Person.java and PersonJpaRepository changed to Jakarta.persistence
    3. In application.properties, added the entry spring.jpa.defer-datasource-initialization=true

--- 04-spring-jdbc-to-jpa/src/main/java/com/in28minutes/database/databasedemo/jpa/PersonJpaRepository.java
+++ 04-spring-jdbc-to-jpa/src/main/java/com/in28minutes/database/databasedemo/jpa/PersonJpaRepository.java
@@ -2,11 +2,10 @@ package com.in28minutes.database.databasedemo.jpa;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 import com.in28minutes.database.databasedemo.entity.Person;
