407c0f3401365ee2a3940d613faa4561862d139d
bsmahi <bs.mahi@gmail.com>
2022-05-04 09:49:05 +0530

    Upgraded to Spring Boot 3.0.0-M2

--- 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -2,6 +2,7 @@ package com.in28minutes.spring.basics.springin5steps.basic;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
